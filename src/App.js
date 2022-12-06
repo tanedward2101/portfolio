ccfc40517fb5fafcf2474b44d3663b874fabcc4f
tanedward21 <tanedward2101@gmail.com>
2022-12-06 21:57:58 +0700

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './component/Navigation';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      </header>
+      <Navigation />
     </div>
   );
 }
